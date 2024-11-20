09664559ac70fd0fbb249a666a194b26419aeff9
PolygonPrincess <polygonprincess@duck.com>
2024-11-19 19:36:07 -0500

    Changed clippy base path to remote url

--- assets/clippyjs/build/clippy.js
+++ assets/clippyjs/build/clippy.js
@@ -844,7 +844,7 @@ clippy.Balloon.prototype = {
 
 };
 
-clippy.BASE_PATH = 'assets/clippyjs/Agents/';
+clippy.BASE_PATH = 'https://raw.githubusercontent.com/PolygonPrincess/polygonprincess.github.io/refs/heads/main/assets/clippyjs/agents/';
 
 clippy.load = function (name, successCb, failCb) {
     var path = clippy.BASE_PATH + name;
